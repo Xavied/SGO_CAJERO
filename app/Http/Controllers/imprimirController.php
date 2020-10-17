a68b159d32e73d4be7d66f62000974ad60d33613
christian-1998-Alexander <christianmadril@gmail.com>
2020-10-17 17:07:54 -0500

    Prueba maquetado de la factura pdf

--- app/Http/Controllers/imprimirController.php
+++ app/Http/Controllers/imprimirController.php
@@ -13,17 +13,12 @@ class imprimirController extends Controller
    
     
     public function imprimir($idFactura){
-        //$pdf = \PDF::loadView('imprimir');
-        //return $pdf->stream('imprimir.pdf');
-
-       // $pdf = PDF::loadView('imprimir');
-        //return $pdf->stream('imprimir.pdf');
+       
 
        $idFac= $idFactura; //extraemos el id que nos llega al buscar una factura
        $iva=0.12;//iva funcional del controlador
        $vistaiva=12;//iva para mostrar en la vista
-     //  $pdf = PDF::loadView('imprimir');
-
+   
       try
 
        {
@@ -62,7 +57,7 @@ class imprimirController extends Controller
                 $pdf = \PDF::loadView('imprimir',compact('facs', 'detalles', 'var', 'vistaiva', 'subtotal','idFac'));
                 return $pdf->stream('imprimir.pdf');
                  //pasamos cada valor a la vista Factura
-
+                 //return $pdf->stream('imprimir.pdf');
                 //, compact('facs', 'detalles', 'var', 'vistaiva', 'subtotal')
         } catch(guzzlehttp \ guzzle \ src \ Exception \ RequestException $e)
         {
@@ -70,67 +65,7 @@ class imprimirController extends Controller
 
         }
 
-       /* public function imprimirFac(){
-            $pdf = PDF::loadView('imprimir',compact('facs','detalles','var','vistaiva','subtotal','idFac'));
-            return $pdf->strean('imprimir.pdf');
-        }
-        //$idFac  = $this->find();
-        //$pdf = PDF::loadView('imprimir');
-        //return $pdf->stream('imprimir.pdf');
-      //$facs = $request->get('accion');
-     
-    }
-    
-   /*public function find(Request $request)
-    {
-       $idFac=$request->idFac;//extraemos el id que nos llega al buscar una factura
-       $iva=0.12;//iva funcional del controlador
-       $vistaiva=12;//iva para mostrar en la vista
-       $pdf = PDF::loadView('imprimir');
-
-      try
-
-       {
-            $client = new Client ([
-                'base_uri'=>'https://safe-bastion-34410.herokuapp.com',
-            //'timeout'=> 2.0,// tiempo a esperar por una respuesta
-
-            ]);
-
-                $response = $client->request('GET', "/api/facs/{$idFac}");//añadimos el número que extraímos a la ruta api/facs/{númeroExtarido}
-                $responsedeta = $client->request('GET', "/api/facs/{$idFac}");//hacemos otra llamada con la misma petición
-
-                $facs= json_decode($response->getBody()->getContents());//extraemos el contenido de facs
-
-                
-                $detalles=json_decode($responsedeta->getBody()->getContents(), true);//array del json
-
-                $detales=$detalles['detalles_de_platos'];//nos concentramos en el array de detalles de platos
-
-                //vemos la longitud
-                $longitud= count($detales);//vemos su longitud
-                $var=0;
-
-                for($i=0; $i<$longitud; $i++) //iteramos en el array
-                {
-                    $var+=$detales[$i]['dtall_valor']; //sumamos los valores de cada iteración
-
-                }
-                //calculamos el total del iva
-                $totalconiva=$var*$iva;
-                //restamos el total menos el iva
-                $subtotaliva=$var-$totalconiva;
-                $subtotal=\number_format($subtotaliva, 2);
-
-                return $pdf->strean('imprimir.pdf', compact('facs', 'detalles', 'var', 'vistaiva', 'subtotal')); //pasamos cada valor a la vista Factura
-
-
-        } catch(guzzlehttp \ guzzle \ src \ Exception \ RequestException $e)
-        {
-            return "No se encuentrá la factura" . $e->getmessage();
-
-        }*/
-
+       
 
 
     }
