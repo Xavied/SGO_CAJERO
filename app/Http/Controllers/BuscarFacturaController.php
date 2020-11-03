22c183622999bcf15d6378a39e8a93c8332d5971
Xavied <64937713+Xavied@users.noreply.github.com>
2020-11-03 16:45:10 -0500

    Se añade los headers de Autorización en los controladores

--- app/Http/Controllers/BuscarFacturaController.php
+++ app/Http/Controllers/BuscarFacturaController.php
@@ -16,10 +16,11 @@ class BuscarFacturaController extends Controller
         try
         {
             $cedula_cli=$request->cli_cedula;
-
+            $token= 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1IiwianRpIjoiNTQyZWRlNWNhZjY1YjM4MGE3MDMyMjIxZjM1NmY4YTQxYjQwY2U3MGU3MDNmYTRiNzViYjk4ZmY0OWVjMDU0NTgwYThiY2I1ODQyMzEyYjkiLCJpYXQiOjE2MDMwNTk4ODYsIm5iZiI6MTYwMzA1OTg4NiwiZXhwIjoxNjM0NTk1ODg1LCJzdWIiOiI0Iiwic2NvcGVzIjpbXX0.VWEVXyX2sH6N9jmbdLeIpkgu1zOGWbS8E9UwEca34ic4j8HsgpN02O99p-cksj37b34Fa8Usf_LAjWRPPLPcQOutZVFKIjrIFgHrQsFDPrgNZHvlEz_DPpQXsvi-XDwFm7wC1pKXwKbQfG8M70RAH-iznlzATff7XMhYC9woWwOlA_YaIonJ8T9UAVGvpcM7Kwa0ck2-sznXeV9GSAjsvM6pGzONmxCQER5UDTCSv8zXmjmNB8U-_x4kRp4IZyaOkdlAFAXo6-60d_ZanduaqfmIYG_W4JF-JbLdUBl6uZRgBSy1KS9yyaYiP7hZb82foTuJbMed8txnOPErMNitSswMKduzduUzAU4XKGn7b4YnnSKfMHVIhNKCGj25vehaFPnvW8N_9mk1I2PdGxq5kRdVGzq9mYtahNR3D17rrvneDW5ECSp7haYhkVVOBnioBo4MmdTyGUdh0e76Y6oieu0v83dKUqXQULG6i8J107KHL0d7yKO2nRfY-ru-F7vknJlsfQtj2jMcZXxK3-Uq3xJrrcUKKsFted9o5iRlJh5af8JEqkal1bbpaKRQAlTYa3JvcNbsMmtDgQjYcK7mxYRU2tKzuf0P1UVh9pJiMH0t3VdaZj3tlPEGKI1BvfZLbB7LV_Y_pVPWgBbn4Ilj8oYuZnTNE0uYbqE13WNBSXI';
             $client = new Client ([
                 'base_uri'=>'https://sgo-central-6to.herokuapp.com',
             //'timeout'=> 2.0,// tiempo a esperar por una respuesta
+                'headers'=>['Authorization'=> $token]
 
             ]);
             $verf= $client->request('GET', "/api/cedulaclientes/$cedula_cli");
@@ -57,22 +58,24 @@ class BuscarFacturaController extends Controller
     public function find(Request $request)
     {
        $idFac=$request->idFac;//extraemos el id que nos llega al buscar una factura
- ChristianM_rama
+
        //$iva=0.12;//iva funcional del controlador
        $vistaiva=0.12;//iva para mostrar en la vista
 
        $idPedido=$request->idPedido;//extreamos el id del pedido que llega
        $iva=0.12;//iva funcional del controlador
        $vistaiva=12;//iva para mostrar en la vista
- master
+
 
 
       try
 
        {
+            $token= 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1IiwianRpIjoiNTQyZWRlNWNhZjY1YjM4MGE3MDMyMjIxZjM1NmY4YTQxYjQwY2U3MGU3MDNmYTRiNzViYjk4ZmY0OWVjMDU0NTgwYThiY2I1ODQyMzEyYjkiLCJpYXQiOjE2MDMwNTk4ODYsIm5iZiI6MTYwMzA1OTg4NiwiZXhwIjoxNjM0NTk1ODg1LCJzdWIiOiI0Iiwic2NvcGVzIjpbXX0.VWEVXyX2sH6N9jmbdLeIpkgu1zOGWbS8E9UwEca34ic4j8HsgpN02O99p-cksj37b34Fa8Usf_LAjWRPPLPcQOutZVFKIjrIFgHrQsFDPrgNZHvlEz_DPpQXsvi-XDwFm7wC1pKXwKbQfG8M70RAH-iznlzATff7XMhYC9woWwOlA_YaIonJ8T9UAVGvpcM7Kwa0ck2-sznXeV9GSAjsvM6pGzONmxCQER5UDTCSv8zXmjmNB8U-_x4kRp4IZyaOkdlAFAXo6-60d_ZanduaqfmIYG_W4JF-JbLdUBl6uZRgBSy1KS9yyaYiP7hZb82foTuJbMed8txnOPErMNitSswMKduzduUzAU4XKGn7b4YnnSKfMHVIhNKCGj25vehaFPnvW8N_9mk1I2PdGxq5kRdVGzq9mYtahNR3D17rrvneDW5ECSp7haYhkVVOBnioBo4MmdTyGUdh0e76Y6oieu0v83dKUqXQULG6i8J107KHL0d7yKO2nRfY-ru-F7vknJlsfQtj2jMcZXxK3-Uq3xJrrcUKKsFted9o5iRlJh5af8JEqkal1bbpaKRQAlTYa3JvcNbsMmtDgQjYcK7mxYRU2tKzuf0P1UVh9pJiMH0t3VdaZj3tlPEGKI1BvfZLbB7LV_Y_pVPWgBbn4Ilj8oYuZnTNE0uYbqE13WNBSXI';
             $client = new Client ([
                 'base_uri'=>'https://sgo-central-6to.herokuapp.com',
             //'timeout'=> 2.0,// tiempo a esperar por una respuesta
+                'headers'=>['Authorization'=> $token]
 
             ]);
 
@@ -99,8 +102,8 @@ class BuscarFacturaController extends Controller
                 //calculamos el total del iva
                // $totalconiva=$var*$iva;
                 //restamos el total menos el iva
-ChristianM_rama
-                
+
+
                 $subtotaliva=$var;
                 $subtotal=\number_format($subtotaliva);
                 $IVA = $subtotal* $vistaiva;
@@ -112,7 +115,7 @@ ChristianM_rama
                 $subtotal=\number_format($subtotaliva, 2);
 
                 return view('Factura', compact('facs', 'detalles', 'var', 'vistaiva', 'subtotal','idFac', 'idPedido')); //pasamos cada valor a la vista Factura
-master
+
 
 
         } catch(guzzlehttp \ guzzle \ src \ Exception \ RequestException $e)
--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -17,72 +17,74 @@ class ReporteController extends Controller
     }
 
     public function excel(Request $request)
-    {        
+    {
         $lol=$request->lol;
         $keys=$request->keys;
         return Excel::download(new ReporteExport($lol, $keys), 'Reporte.xlsx');
     }
-    
+
     public function crearReporte(Request $request)
-    {       
-        
+    {
+
         $fecha=$request->fecha;//almacena la fecha que envia el usuario
+        $token= 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI1IiwianRpIjoiNTQyZWRlNWNhZjY1YjM4MGE3MDMyMjIxZjM1NmY4YTQxYjQwY2U3MGU3MDNmYTRiNzViYjk4ZmY0OWVjMDU0NTgwYThiY2I1ODQyMzEyYjkiLCJpYXQiOjE2MDMwNTk4ODYsIm5iZiI6MTYwMzA1OTg4NiwiZXhwIjoxNjM0NTk1ODg1LCJzdWIiOiI0Iiwic2NvcGVzIjpbXX0.VWEVXyX2sH6N9jmbdLeIpkgu1zOGWbS8E9UwEca34ic4j8HsgpN02O99p-cksj37b34Fa8Usf_LAjWRPPLPcQOutZVFKIjrIFgHrQsFDPrgNZHvlEz_DPpQXsvi-XDwFm7wC1pKXwKbQfG8M70RAH-iznlzATff7XMhYC9woWwOlA_YaIonJ8T9UAVGvpcM7Kwa0ck2-sznXeV9GSAjsvM6pGzONmxCQER5UDTCSv8zXmjmNB8U-_x4kRp4IZyaOkdlAFAXo6-60d_ZanduaqfmIYG_W4JF-JbLdUBl6uZRgBSy1KS9yyaYiP7hZb82foTuJbMed8txnOPErMNitSswMKduzduUzAU4XKGn7b4YnnSKfMHVIhNKCGj25vehaFPnvW8N_9mk1I2PdGxq5kRdVGzq9mYtahNR3D17rrvneDW5ECSp7haYhkVVOBnioBo4MmdTyGUdh0e76Y6oieu0v83dKUqXQULG6i8J107KHL0d7yKO2nRfY-ru-F7vknJlsfQtj2jMcZXxK3-Uq3xJrrcUKKsFted9o5iRlJh5af8JEqkal1bbpaKRQAlTYa3JvcNbsMmtDgQjYcK7mxYRU2tKzuf0P1UVh9pJiMH0t3VdaZj3tlPEGKI1BvfZLbB7LV_Y_pVPWgBbn4Ilj8oYuZnTNE0uYbqE13WNBSXI';
         $client = new Client ([
             'base_uri'=>'https://sgo-central-6to.herokuapp.com',
         //'timeout'=> 2.0,// tiempo a esperar por una respuesta
-        ]);
+            'headers'=>['Authorization'=> $token]
 
+        ]);
             $todasfacs = $client->request('GET', "/api/facs");//solicita todas las facturas
             $todas=json_decode($todasfacs->getBody()->getContents(), true);//se pone todas las facturas en un array
-            $todas = $todas['data:'];//se obtiene solo el array de datos            
+            $todas = $todas['data:'];//se obtiene solo el array de datos
 
             $todasout = [];
 
             //--------------------------------------------------------------
             //$givenDate = "2020-10-10";
             $givenDate = $fecha;
-            //$dateMinusOneWeek = Carbon::parse($givenDate)->subWeek()->format('Y-m-d');            
+            //$dateMinusOneWeek = Carbon::parse($givenDate)->subWeek()->format('Y-m-d');
             $datePlusOneWeek = Carbon::parse($givenDate)->addWeek()->format('Y-m-d');
 
-            $period = CarbonPeriod::create($givenDate , $datePlusOneWeek);          
-            
+            $period = CarbonPeriod::create($givenDate , $datePlusOneWeek);
+
             $dates = array();
             foreach ($period as $date) {
                     $dates[] = $date->format('Y-m-d');
-                }           
-            
+                }
+
             //Obtiene los ids de las facturas generadas en esas fechas
             foreach($dates as $date)
-                foreach($todas as $element) 
+                foreach($todas as $element)
                 {
                     if($element['fct_fch']==$date)
                     {
                         $todasout[$element['fct_fch']][] = ['id' => $element['id']];
-                    }                        
-                }           
+                    }
+                }
 
             $todatos = [];
             $i=0;
 
             //Obtiene todos los datos de las facturas de una fecha de todaout2
-            foreach($todasout as $ele) 
+            foreach($todasout as $ele)
             foreach($ele as $element)
             {
-                $auxi = $client->request('GET', "/api/facs/{$element['id']}"); //llamada por id a cada factura 
-                $auxide=json_decode($auxi->getBody()->getContents(), true);   
+                $auxi = $client->request('GET', "/api/facs/{$element['id']}"); //llamada por id a cada factura
+                $auxide=json_decode($auxi->getBody()->getContents(), true);
                 $todatos[$i]=$auxide;  //se almacena cada factura en una posición del arreglo
-                $i++;               
-            }     
-            
+                $i++;
+            }
+
             //obtiene el nombre y la cantidad de todos los detalles en las facturas
-            $Detalle = [];  
+            $Detalle = [];
             $j=0;
-            
+
             foreach($todatos as $element)
                 foreach($element['detalles_de_platos'] as $element1)
-                {   
-                    $Detalle[] = ['fct_fch' =>$element['data']['fct_fch'], 'plt_nom' => $element1['plt_nom'],'dtall_cant' => $element1['dtall_cant']+0, 'plt_pvp'=> $element1['plt_pvp']+0];                                   
-                } 
+                {
+                    $Detalle[] = ['fct_fch' =>$element['data']['fct_fch'], 'plt_nom' => $element1['plt_nom'],'dtall_cant' => $element1['dtall_cant']+0, 'plt_pvp'=> $element1['plt_pvp']+0];
+                }
 
             $out = array();
             foreach ($Detalle as $row) {
@@ -103,20 +105,20 @@ class ReporteController extends Controller
 
             $lol=array();
             foreach($out2 as $element1)
-            {   
-                $lol[$element1['fct_fch']][] = $element1;                                   
-            }  
+            {
+                $lol[$element1['fct_fch']][] = $element1;
+            }
 
-            $keys = array_keys($lol);                
-                
+            $keys = array_keys($lol);
+
             #region Arreglos para Gráficos
                 //obtiene el nombre y la cantidad de todos los detalles en las facturas
-                $DetalleGraf = [];            
-                foreach($todatos as $element)                
+                $DetalleGraf = [];
+                foreach($todatos as $element)
                     foreach($element['detalles_de_platos'] as $element1)
-                    {   
-                        $DetalleGraf[$element1['plt_nom']][] = ['plt_nom' => $element1['plt_nom'],'dtall_cant' => $element1['dtall_cant']+0];                                   
-                    }           
+                    {
+                        $DetalleGraf[$element1['plt_nom']][] = ['plt_nom' => $element1['plt_nom'],'dtall_cant' => $element1['dtall_cant']+0];
+                    }
 
                 $agrupGraf = [];
                 $j=0;
@@ -132,11 +134,11 @@ class ReporteController extends Controller
                         }
                         $l=0;
                         $j++;
-                    }     
+                    }
             #endregion
-     
-            
-        return view('Reporte', compact('lol', 'keys', 'agrupGraf', 'dates')); 
+
+
+        return view('Reporte', compact('lol', 'keys', 'agrupGraf', 'dates'));
     }
-    
+
 }
