c1025765ea5602b2ec555f28e6b3aff2f739c78b
Christian Vicente <christianvicenter@hotmail.com>
2020-11-04 00:30:43 -0500

    Se modificó la página de inicio del Admin

--- resources/views/webadmin/homeadmin.blade.php
+++ resources/views/webadmin/homeadmin.blade.php
@@ -1,3 +1,7 @@
 @extends('layouts.adminly')
 @section('content')
+<div class="text-center">
+    <h1>¡Bienvenido!</h1>
+    <h2>Accede con tus credenciales de administrador</h2>
+</div>
 @endsection
