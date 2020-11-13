0b99a69b4eb4c0caa1ecc59098bc89cea7d0f844
Xavied <64937713+Xavied@users.noreply.github.com>
2020-11-12 23:33:16 -0500

    Creacion de layout para cajero y cambio de css en webcajero

--- resources/views/webcajero/welcome.blade.php
+++ resources/views/webcajero/welcome.blade.php
@@ -1,95 +1,53 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.cajeroly')
+@section('content')
+                <div class="container mt-5">
+                    <div class="row justify-content-center">
+                        <div class="col-md-8">
+                            <div class="card">
+                                <div class="card-body">
+                                    <form action="/welcome" method="POST">
+                                        {{csrf_field()}}
 
-        <title>Laravel</title>
+                                        <div class="form-group row">
+                                            <label for="email" class="col-md-4 col-form-label text-md-right">Usuario</label>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <!-- UIkit CSS -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.4.6/dist/css/uikit.min.css" />
+                                            <div class="col-md-6">
+                                                <input id="email" type="email" class="form-control" name="email" required autocomplete="current-email" autofocus>
+                                                <!--pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}"-->
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <label for="password" class="col-md-4 col-form-label text-md-right">Contraseña</label>
+
+                                            <div class="col-md-6">
+                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" autofocus>
+
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+                                        </div>
 
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #FFBC30;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                   SGO
-                </div>
-                <form action="/welcome" method="POST">
-                    {{csrf_field()}}
-                     <div class="uk-margin">
-                            <input class="uk-input uk-form-width-medium" name="email" type="text" placeholder="correo">
-                     </div>
-                     <div class="uk-margin">
-                            <input class="uk-input uk-form-width-medium" type="password" name="password" type="text" placeholder="contraseña">
-                     </div>
-                     <div  class="uk-card uk-card-default uk-card-body uk-animation-fade">
-                         <p class="uk-text-center">
-                             <button class="uk-button uk-button-default uk-text-warning" type=submit>Iniciar Sesión</button>
-                         </p>
+                                        <div class="form-group row mb-0">
+                                            <div class="col-md-8 offset-md-4">
+                                                <button type="submit" class="btn btn-dark">
+                                                    Acceder
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                </div>
 
-                </form>
+@endsection
 
 
-            </div>
-        </div>
-    </body>
-</html>
+
