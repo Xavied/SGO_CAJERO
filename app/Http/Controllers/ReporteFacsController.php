7fd6675cc1922d5ba5a2fe3d965915e06c36ac29
salop72 <salop72@hotmail.com>
2020-12-01 14:41:53 -0500

    Mensaje de error Reporte Facturas

--- app/Http/Controllers/ReporteFacsController.php
+++ app/Http/Controllers/ReporteFacsController.php
@@ -45,6 +45,12 @@ class ReporteFacsController extends Controller
             'headers'=>['Authorization'=> $token]
 
         ]);
+
+        if (empty($fechaR)) {
+            $MensajeError = "";
+                $MensajeErrorFacs = "Debe ingresar una fecha válida";
+                return view('solicReporte', compact('MensajeError', 'MensajeErrorFacs'));
+        }
             $facs = $client->request('GET', "/api/facs");//solicita todas las facturas
             $todasfacs=json_decode($facs->getBody()->getContents(), true);//pone todas las facturas en un array
             $todasfacs = $todasfacs['data:'];//obtiene solo el array de datos 
